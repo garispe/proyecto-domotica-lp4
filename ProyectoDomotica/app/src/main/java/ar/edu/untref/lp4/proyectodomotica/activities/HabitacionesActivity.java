e580e777db160e32e76a62c02fb6a2becaca1642
GuillermoArispe <guillermo.arispe@gmail.com>
2015-05-01 19:23:52 -0300

    Se agregan portada e icono

--- ProyectoDomotica/app/src/main/java/ar/edu/untref/lp4/proyectodomotica/activities/HabitacionesActivity.java
+++ ProyectoDomotica/app/src/main/java/ar/edu/untref/lp4/proyectodomotica/activities/HabitacionesActivity.java
@@ -1,41 +1,16 @@
 package ar.edu.untref.lp4.proyectodomotica.activities;
 
-import android.support.v7.app.ActionBarActivity;
+import android.app.Activity;
 import android.os.Bundle;
-import android.view.Menu;
-import android.view.MenuItem;
 
 import ar.edu.untref.lp4.proyectodomotica.R;
 
-
-public class HabitacionesActivity extends ActionBarActivity {
+public class HabitacionesActivity extends Activity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
-    }
+        setContentView(R.layout.activity_habitaciones);
 
-
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_main, menu);
-        return true;
-    }
-
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
-
-        //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
-            return true;
-        }
-
-        return super.onOptionsItemSelected(item);
     }
 }
--- ProyectoDomotica/app/src/main/java/ar/edu/untref/lp4/proyectodomotica/activities/PortadaActivity.java
+++ ProyectoDomotica/app/src/main/java/ar/edu/untref/lp4/proyectodomotica/activities/PortadaActivity.java
@@ -1,5 +1,8 @@
 package ar.edu.untref.lp4.proyectodomotica.activities;
 
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Handler;
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
 import android.view.Menu;
@@ -7,34 +10,31 @@ import android.view.MenuItem;
 
 import ar.edu.untref.lp4.proyectodomotica.R;
 
-public class PortadaActivity extends ActionBarActivity {
+public class PortadaActivity extends Activity {
+
+    private static final int DURACION = 3000; // 3 segundos
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_portada);
+
+        Handler h = new Handler();
+
+        h.postDelayed(new Runnable() {
+
+            @Override
+            public void run() {
+
+                abrirMenu();
+            }
+        }, DURACION);
     }
 
+    private void abrirMenu(){
 
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_portada, menu);
-        return true;
-    }
-
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
-
-        //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
-            return true;
-        }
-
-        return super.onOptionsItemSelected(item);
+        Intent intent = new Intent(this, HabitacionesActivity.class);
+        startActivity(intent);
+        finish();
     }
 }

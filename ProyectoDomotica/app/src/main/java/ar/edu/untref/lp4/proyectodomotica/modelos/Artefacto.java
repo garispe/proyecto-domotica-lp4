9ce92d555030a0dbf09a27218f1a217b9328eab4
GuillermoArispe <guillermo.arispe@gmail.com>
2015-05-01 18:06:50 -0300

    Se crean entidades

--- ProyectoDomotica/app/src/main/java/ar/edu/untref/lp4/proyectodomotica/modelos/Artefacto.java
+++ ProyectoDomotica/app/src/main/java/ar/edu/untref/lp4/proyectodomotica/modelos/Artefacto.java
@@ -1,4 +1,43 @@
 package ar.edu.untref.lp4.proyectodomotica.modelos;
 
 public class Artefacto {
+
+    private int id;
+    private String nombre;
+    private boolean activo;
+
+    public Artefacto(String nombre) {
+        //ver si hace falta setear id
+        this.nombre = nombre;
+        this.activo = false;
+    }
+
+    public int getId() {
+
+        return this.id;
+
+    }
+
+    public String getNombre() {
+
+        return nombre;
+
+    }
+
+    public void setNombre(String nombre) {
+
+        this.nombre = nombre;
+
+    }
+
+
+    public boolean isActivo() {
+        return activo;
+    }
+
+    public void setActivo(boolean activo) {
+        this.activo = activo;
+    }
+
+
 }
--- ProyectoDomotica/app/src/main/java/ar/edu/untref/lp4/proyectodomotica/modelos/Habitacion.java
+++ ProyectoDomotica/app/src/main/java/ar/edu/untref/lp4/proyectodomotica/modelos/Habitacion.java
@@ -1,4 +1,49 @@
 package ar.edu.untref.lp4.proyectodomotica.modelos;
 
+import java.util.LinkedList;
+import java.util.List;
+
 public class Habitacion {
+
+    private int id;
+    private String nombre;
+    private List<Artefacto> listaArtefactos;
+
+    public Habitacion(String nombre) {
+
+        //ver si hace falta setear id
+        this.nombre = nombre;
+        listaArtefactos = new LinkedList<Artefacto>();
+
+    }
+
+    public int getId() {
+
+        return this.id;
+
+    }
+
+    public String getNombre() {
+
+        return nombre;
+
+    }
+
+    public void setNombre(String nombre) {
+
+        this.nombre = nombre;
+
+    }
+
+    public List<Artefacto> getArtefactos() {
+
+        return this.listaArtefactos;
+
+    }
+
+    public void agregarArtefacto(Artefacto artefactoAAgregar) {
+
+        this.listaArtefactos.add(artefactoAAgregar);
+
+    }
 }

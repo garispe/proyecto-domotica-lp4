631210f29a5ff089fa9870befbe746544feb294b
MartinBeltrame <tinchobeltrame@gmail.com>
2015-06-08 11:31:27 -0300

    Se agregaron funciones a botones de ConfiguracionActivity

--- ProyectoDomotica/app/src/main/java/ar/edu/untref/lp4/proyectodomotica/activities/ConfiguracionActivity.java
+++ ProyectoDomotica/app/src/main/java/ar/edu/untref/lp4/proyectodomotica/activities/ConfiguracionActivity.java
@@ -1,11 +1,25 @@
 package ar.edu.untref.lp4.proyectodomotica.activities;
 
+import android.content.DialogInterface;
+import android.content.res.Configuration;
 import android.os.Bundle;
 import android.support.v7.app.ActionBarActivity;
+import android.support.v7.app.AlertDialog;
 import android.view.View;
+import android.widget.EditText;
 import android.widget.ImageButton;
+import android.widget.LinearLayout;
+import android.widget.TextView;
+import android.widget.Toast;
 
+import org.w3c.dom.Text;
+
+import java.util.Locale;
+
+import ar.edu.untref.lp4.proyectodomotica.BuildConfig;
 import ar.edu.untref.lp4.proyectodomotica.R;
+import ar.edu.untref.lp4.proyectodomotica.controladores.ControladorBaseDatos;
+import ar.edu.untref.lp4.proyectodomotica.modelos.Artefacto;
 
 public class ConfiguracionActivity extends ActionBarActivity {
 
@@ -31,12 +45,31 @@ public class ConfiguracionActivity extends ActionBarActivity {
         bVaciarBD.setImageResource(R.drawable.tacho);
         bDomotica.setImageResource(R.drawable.info_domotica);
 
+        bDomotica.setOnClickListener(new View.OnClickListener() {
+
+            @Override
+            public void onClick(View v) {
+                android.app.AlertDialog.Builder builder = new android.app.AlertDialog.Builder(ConfiguracionActivity.this);
+                builder.setMessage(R.string.mensaje_domotica)
+                        .setTitle(getString(R.string.domotica))
+                        .setCancelable(false)
+                        .setNeutralButton(getString(R.string.entendido),
+                                new DialogInterface.OnClickListener() {
+                                    public void onClick(DialogInterface dialog, int id) {
+                                        dialog.cancel();
+                                    }
+                                });
+                android.app.AlertDialog alert = builder.create();
+                alert.show();
+            }
+        });
+
         bVaciarBD.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
 
                 // Borra la base de datos y cambia la imagen
-                if(!tachoLleno){
+                if (!tachoLleno) {
 
                     bVaciarBD.setImageResource(R.drawable.tacho_lleno);
                     tachoLleno = true;
@@ -46,6 +79,23 @@ public class ConfiguracionActivity extends ActionBarActivity {
                     bVaciarBD.setImageResource(R.drawable.tacho);
                     tachoLleno = false;
                 }
+            }
+        });
+
+        bEspanol.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                Toast.makeText(ConfiguracionActivity.this, "Proximamente lenguaje en espanol", Toast.LENGTH_SHORT).show();
+
+            }
+        });
+
+        bIngles.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                Toast.makeText(ConfiguracionActivity.this, "Proximamente lenguaje en ingles", Toast.LENGTH_SHORT).show();
             }
         });
     }
